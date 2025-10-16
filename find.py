30cf5aa2d5d7b0ee35bf96110e6f4a2ca6bd08c8
RuslanTopal <baranotik15@gmail.com>
2025-10-16 23:38:56 +0000

    modify find file

--- find.py
+++ find.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 if len(sys.argv) != 2:
-    print("Используйте в терминале: python find.py <имя_инстанса>")
+    print("ERROR: You must provide the instance name! Usage: python find.py <instance_name>")
     sys.exit(1)
 
 
@@ -14,7 +14,7 @@ file_path = Path("instances.json")
 
 
 if not file_path.exists():
-    print(f"Файл {file_path} не найден")
+    print(f"ERROR: File {file_path} does not exist! Place it here and try again.")
     sys.exit(1)
 
 
@@ -22,16 +22,16 @@ with file_path.open(encoding="utf-8") as f:
     instances = json.load(f)
 
 found = False
+
 for instance in instances:
     tags = instance.get("Tags", {})
     name = tags.get("Name", "")
 
     if name and target_name in name.lower():
-        print("Данные найдены:")
+        print("SUCCESS: Matching instance found!")
         print(json.dumps(instance, indent=2, ensure_ascii=False))
         found = True
         break
 
 if not found:
-    print(f"Данные с Name, содержащим '{target_name}', не найден")
-
+    print(f"WARNING: No instances found with Name containing '{target_name}'!")
